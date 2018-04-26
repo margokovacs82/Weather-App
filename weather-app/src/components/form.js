8adbd763b3d7d37a06aef9e5618780caeebc0383
margokovacs82 <margo.kovacs@gmail.com>
2018-04-26 15:04:15 +0200

    css fixed

--- weather-app/src/components/form.js
+++ weather-app/src/components/form.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const Form = props => (
-      <div> 
+const Form = props => ( 
         <form className="form-container" onSubmit={props.getWeather}>
         <input type="text" name="city" placeholder="City..."/>
         <input type="text" name="country" placeholder="Country..."/>
         <button>Get weather</button>
         </form>
-      </div>
     );
 
 export default Form;
--- weather-app/src/components/titles.js
+++ weather-app/src/components/titles.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Titles = () => (
-      <div> 
+      <span> 
         <h1 className="title-container__title">Weather finder</h1>
         <h3 className="title-container__subtitle">Find out temperature, conditions and more</h3>
-      </div>
+      </span>
     );
  
 export default Titles;
